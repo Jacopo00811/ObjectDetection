add61b1907b1fd01be6860cfde26ac6676a52410
aka-lowe <s215170@student.dtu.dk>
2024-11-05 21:06:47 +0100

    Dataloader working on trainset, testfile included TODO: implement trainset, and ext val set

--- Dataloader.py
+++ Dataloader.py
@@ -1,24 +1,46 @@
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 import os
 from PIL import Image
 
 class CroppedProposalDataset(Dataset):
-    def __init__(self, dir, mode, transform=None):
+    def __init__(self, mode, transform=None, dir='Potholes/annotated-images'):
+        # split dir by / and join list with current working directory
+        path_list = dir.split('/')
+        dir = os.path.join(os.getcwd(), *path_list)
+
         if mode == 'train':
             self.dir = os.path.join(dir, 'train')
-            self.pos_dir = os.path.join(self.dir, 'positive')
-            self.neg_dir = os.path.join(self.dir, 'background')
-            self.pos_images = [os.path.join(self.pos_dir, f) for f in os.listdir(self.pos_dir)]
-            self.neg_images = [os.path.join(self.neg_dir, f) for f in os.listdir(self.neg_dir)]        
-            self.images = self.pos_images + self.neg_images    
+            self.img_dirs = [os.path.join(self.dir, f) 
+                             for f in os.listdir(self.dir) if os.path.isdir(os.path.join(self.dir, f)) # Collects the list of image directories 
+                             ]
+            self.pos_dirs = [os.path.join(d, 'positive') for d in self.img_dirs] # adds the positive directory to the list of image directories
+            self.neg_dirs = [os.path.join(d, 'background') for d in self.img_dirs] # adds the background directory to the list of image directories
+            # self.pos_dir = os.path.join(self.dir, 'positive') # doesnt work for multiple directories
+            # self.neg_dir = os.path.join(self.dir, 'background') # doesnt work for multiple directories
+
+
+            self.pos_images = [os.path.join(d, f) for d in self.pos_dirs for f in os.listdir(d)] # collects all the positive images
+            self.neg_images = [os.path.join(d, f) for d in self.neg_dirs for f in os.listdir(d)] # collects all the negative images
+
+            # self.pos_images = [os.path.join(self.pos_dir, f) for f in os.listdir(self.pos_dir)] # doesnt work for multiple directories
+            # self.neg_images = [os.path.join(self.neg_dir, f) for f in os.listdir(self.neg_dir)] # doesnt work for multiple directories 
+                  
+            self.images = self.pos_images + self.neg_images
+            self.labels = [1] * len(self.pos_images) + [0] * len(self.neg_images)
         elif mode == 'val':
             pass
         else:
-            self.dir = os.path.join(dir, 'test')
-            self.images = [os.path.join(self.dir, f) for f in os.listdir(self.dir)]
+            # self.dir = os.path.join(dir, 'test')
+            # self.images = [os.path.join(self.dir, f) for f in os.listdir(self.dir)]
+            pass # Pass for now as Proposals are only on training data
 
-        self.transform = transform
-    # TODO: ADD LABELS and complete dataloader
+        # If no transform is specified, apply a default transform to convert to tensor
+        self.transform = transform if transform else transforms.Compose([
+            transforms.Resize((256, 256)),  # Resize to 256x256
+            transforms.ToTensor()           # Convert to tensor
+        ])
+
     def __len__(self):
         return len(self.images)
 
--- crop_dataset.py
+++ crop_dataset.py
@@ -31,18 +31,18 @@ def save_proposals(images, annotations, names, output_dir, num_proposals, iou_th
         # Free up memory
         img = img_dir + '.jpg'
         xml = img_dir + '.xml'
-        # os.remove(img) # TODO: Uncomment this line when ready to run the full dataset
+        os.remove(img) # TODO: Uncomment this line when ready to run the full dataset
         # Move the xml to the new directory?? 
         # shutil.move(xml, pos_dir)
     print(f"Saved cropped proposals in {output_dir}!")
 
 
-# script_dir = os.path.dirname(os.path.abspath(__file__))
-# folder_path = os.path.join(script_dir, 'Potholes', 'annotated-images', 'train')
-# images, annotations, names = read_images_and_xml(folder_path)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+folder_path = os.path.join(script_dir, 'Potholes', 'annotated-images', 'test')
+images, annotations, names = read_images_and_xml(folder_path)
 
 # # Test on a subset of the data
 # images = images[:1]
 # annotations = annotations[:1]
 # names = names[:1]
-# save_proposals(images, annotations, names, output_dir=folder_path, num_proposals=1000, iou_threshold=0.6, algorithm='SelectiveSearch')
+save_proposals(images, annotations, names, output_dir=folder_path, num_proposals=1000, iou_threshold=0.6, algorithm='SelectiveSearch')
