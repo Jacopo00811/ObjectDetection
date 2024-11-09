20677198a8dfd7e757bae4c76981fa03f3d6f22c
aka-lowe <s215170@student.dtu.dk>
2024-11-09 15:24:19 +0100

    changes

--- Dataloader.py
+++ Dataloader.py
@@ -1,55 +1,166 @@
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
+from torchvision.transforms import v2 as transforms
 import os
 from PIL import Image
+import random
+import torch
 
 class CroppedProposalDataset(Dataset):
-    def __init__(self, mode, transform=None, dir='Potholes/annotated-images'):
-        # split dir by / and join list with current working directory
+    def __init__(self, mode, transform, dir='Potholes/annotated-images', size=256):
         path_list = dir.split('/')
         dir = os.path.join(os.getcwd(), *path_list)
-
+        self.transform = transform
+        self.size = size
         if mode == 'train':
             self.dir = os.path.join(dir, 'train')
-            self.img_dirs = [os.path.join(self.dir, f) 
-                             for f in os.listdir(self.dir) if os.path.isdir(os.path.join(self.dir, f)) # Collects the list of image directories 
-                             ]
-            self.pos_dirs = [os.path.join(d, 'positive') for d in self.img_dirs] # adds the positive directory to the list of image directories
-            self.neg_dirs = [os.path.join(d, 'background') for d in self.img_dirs] # adds the background directory to the list of image directories
-            # self.pos_dir = os.path.join(self.dir, 'positive') # doesnt work for multiple directories
-            # self.neg_dir = os.path.join(self.dir, 'background') # doesnt work for multiple directories
-
-
-            self.pos_images = [os.path.join(d, f) for d in self.pos_dirs for f in os.listdir(d)] # collects all the positive images
-            self.neg_images = [os.path.join(d, f) for d in self.neg_dirs for f in os.listdir(d)] # collects all the negative images
-
-            # self.pos_images = [os.path.join(self.pos_dir, f) for f in os.listdir(self.pos_dir)] # doesnt work for multiple directories
-            # self.neg_images = [os.path.join(self.neg_dir, f) for f in os.listdir(self.neg_dir)] # doesnt work for multiple directories 
-                  
-            self.images = self.pos_images + self.neg_images
-            self.labels = [1] * len(self.pos_images) + [0] * len(self.neg_images)
         elif mode == 'val':
-            pass
+            self.dir = os.path.join(dir, 'validation')
+        elif mode == 'test':
+            self.dir = os.path.join(dir, 'test')
         else:
-            # self.dir = os.path.join(dir, 'test')
-            # self.images = [os.path.join(self.dir, f) for f in os.listdir(self.dir)]
-            pass # Pass for now as Proposals are only on training data
+            raise ValueError("\nInvalid mode. Please choose 'train', 'val', or 'test'\n")
 
-        # If no transform is specified, apply a default transform to convert to tensor
-        self.transform = transform if transform else transforms.Compose([
-            transforms.Resize((256, 256)),  # Resize to 256x256
-            transforms.ToTensor()           # Convert to tensor
-        ])
+
+        self.img_dirs = [os.path.join(self.dir, f) 
+                            for f in os.listdir(self.dir) if os.path.isdir(os.path.join(self.dir, f))] # Collects the list of image directories 
+        self.list_of_images = [] 
+        self.list_of_labels = []
+        self.list_of_xml_dir = []
+        
+        for image_dir in self.img_dirs:
+            self.pos_dir = os.path.join(image_dir, 'positive')
+            self.neg_dir = os.path.join(image_dir, 'background')
+            self.xml_dir = os.path.join(image_dir, image_dir+".xml")
+
+            self.pos_images = [os.path.join(self.pos_dir, f) for f in os.listdir(self.pos_dir)]
+            self.neg_images = [os.path.join(self.neg_dir, f) for f in os.listdir(self.neg_dir)]
+            
+            if mode == 'test':
+                # No upsampling for test data
+                number = len(self.neg_images)
+                self.neg_images = self.neg_images[:number*3]
+                self.images = self.pos_images + self.neg_images
+                self.labels = [1] * len(self.pos_images) + [0] * len(self.neg_images)
+                self.list_of_images.append(self.images)
+                self.list_of_labels.append(self.labels)
+            else:
+                # Shuffle negatives 
+                random.shuffle(self.neg_images)
+                self.neg_images = self.neg_images[:24]
+
+                self.images, self.labels = self.upsample_if_needed(self.transform, self.pos_images, self.neg_images)
+                self.list_of_images.append(self.images)
+                self.list_of_labels.append(self.labels)
+            self.list_of_xml_dir.append(self.xml_dir)
+        
+        # Remove empty lists if any
+        self.list_of_images = [lst for lst in self.list_of_images if lst]
+        self.list_of_labels = [lst for lst in self.list_of_labels if lst]
+        self.list_of_xml_dir = [lst for lst in self.list_of_xml_dir if lst]
+
+        # Transforms applied before output 
+        self.transform =  transforms.Compose([ 
+            transforms.Resize((self.size, self.size)),
+            transforms.ToImage(),
+            transforms.ToDtype(torch.float32, scale=True)])
+
+    def upsample_if_needed(self, transform, pos_images, neg_images):
+        upsampled_pos_images = []
+        if len(pos_images) == 0:
+            return [], []
+        # If there are fewer than 8 positive images, apply transformations to reach 8 images
+        if len(pos_images) > 0 and len(pos_images) < 8:
+            upsampled_pos_images.extend(pos_images)
+            # Apply transformations to positive images until we reach 8 in total
+            while len(upsampled_pos_images) < 8:
+                for img in pos_images:
+                    if len(upsampled_pos_images) < 8:
+                        upsampled_pos_images.append(transform(img))
+                    else:
+                        break
+        else:
+            upsampled_pos_images = pos_images[:8]  # If 8 or more positives get the first 8 images
+
+        images = upsampled_pos_images + neg_images        
+        labels = [1] * len(upsampled_pos_images) + [0] * len(neg_images)
+
+        return images, labels
 
     def __len__(self):
-        return len(self.images)
+        return len(self.list_of_images)
 
     def __getitem__(self, idx):
-        image_path = self.images[idx]
-        label = self.labels[idx]
-        image = Image.open(image_path).convert("RGB")
+        # Get the entire list of proposals and labels for a single image directory
+        image_paths = self.list_of_images[idx]  # List of paths for proposals
+        labels = self.list_of_labels[idx]  # List of corresponding labels
+        xml_dir = self.list_of_xml_dir[idx] # .xml file's name
+        
+        combined = list(zip(image_paths, labels))
+        random.shuffle(combined)
+        image_paths, labels = zip(*combined)
 
+        images = [Image.open(image_path).convert("RGB") for image_path in image_paths]
+        # Apply the output transformations to each image
         if self.transform:
-            image = self.transform(image)
+            images = [self.transform(image) for image in images]
 
-        return image, label
+        return torch.stack(images), torch.tensor(labels), xml_dir
+
+transform = transforms.Compose([
+    transforms.RandomVerticalFlip(p=0.5),
+    transforms.ColorJitter(brightness=0.3, saturation=0.3),
+    transforms.RandomAutocontrast(p=0.5),
+    transforms.RandomAdjustSharpness(2, p=0.5),
+])
+
+train_dataset = CroppedProposalDataset('train', transform=transform, size=256)
+train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False) # !! Do not shuffle here and do not change batch_size !!
+
+
+################################## TESTING ##################################
+
+import matplotlib.pyplot as plt
+
+for batch_idx, (images, labels, xml_dir) in enumerate(train_loader):
+    images = images.squeeze(0)  # Remove the batch dimension when batch_size is 1
+    labels = labels.squeeze(0)  # Remove the batch dimension when batch_size is 1
+    xml_dir = xml_dir[0]  # Remove the tuple
+
+    print(f"Batch {batch_idx + 1}:")
+    print(f"  Number of images in batch: {len(images)}")  
+    print(f"  Number of labels in batch: {len(labels)}")
+    print(f"  Labels: {labels}")
+    print(f"  Shape of images: {images.shape}")
+    print(f"  XML file: {xml_dir}")
+    image_name = xml_dir.split('\\')[-1].split('.')[0] + '.jpg'
+
+
+    # print(f"Example image: {images[0]}")
+
+    # # Plot images in the first batch
+    if batch_idx == 6:
+
+        plt.figure(figsize=(20, 20))
+        for i in range(len(images)):
+            img = images[i].permute(1, 2, 0).cpu().numpy()  # Convert to HWC format for plotting
+            plt.subplot(4, 8, i + 1)
+            plt.imshow(img)
+            plt.axis('off')
+
+            # Add label text on the image
+            label = labels[i].item()  # Convert label tensor to a scalar
+            plt.text(5, 5, f"Label: {label}", color='white', fontsize=12, ha='left', va='top', backgroundcolor='black')
+        plt.savefig('batch_with_labels.png')
+        
+
+        print(os.path.join("Potholes", "annotated-images", "train", image_name))
+        image = Image.open(os.path.join("Potholes", "annotated-images", "train", image_name))
+        plt.figure(figsize=(10, 10))
+        plt.imshow(image)
+        plt.axis('off')
+        plt.title("Original Image")
+        plt.savefig('original_image.png')
+        break
+
+
+
--- crop_dataset.py
+++ crop_dataset.py
@@ -3,9 +3,69 @@ from turtle import pos
 from read_XML import *
 from object_proposal import SelectiveSearch, EdgeDetection
 from evaluate_proposal import compute_IoU
+import xml.etree.ElementTree as ET
+import os
+from xml.dom import minidom
+from tqdm import tqdm
+
+
+def initialize_xml_file(output_dir, img_name, img_shape):
+    # Set up root annotation element
+    annotation = ET.Element("annotation")
+    
+    # Add folder, filename, and path
+    ET.SubElement(annotation, "folder").text = output_dir
+    ET.SubElement(annotation, "filename").text = img_name
+    ET.SubElement(annotation, "path").text = os.path.join(output_dir, img_name)
+    
+    # Add source element with database child
+    source = ET.SubElement(annotation, "source")
+    ET.SubElement(source, "database").text = "Unknown"
+    
+    # Add size element with width, height, and depth
+    size = ET.SubElement(annotation, "size")
+    ET.SubElement(size, "width").text = str(img_shape[1])  # Width of the image
+    ET.SubElement(size, "height").text = str(img_shape[0])  # Height of the image
+    ET.SubElement(size, "depth").text = str(img_shape[2]) if len(img_shape) > 2 else "1"  # Depth
+    
+    # Add segmented element
+    ET.SubElement(annotation, "segmented").text = "0"
+    
+    # Save the XML tree as an empty structure to the file
+    tree = ET.ElementTree(annotation)
+    xml_file_path = os.path.join(output_dir, img_name.replace('.jpg', '.xml'))
+    tree.write(xml_file_path, encoding="utf-8", xml_declaration=True)
+
+    return xml_file_path
+
+def add_object_to_xml(xml_file_path, box, proposal_name, label="predicted_object"):
+    # Load the existing XML file
+    tree = ET.parse(xml_file_path)
+    root = tree.getroot()
+    
+    # Create the object element
+    object_element = ET.Element("object-proposal")
+    ET.SubElement(object_element, "name").text = label
+    ET.SubElement(object_element, "proposal").text = proposal_name
+    ET.SubElement(object_element, "pose").text = "Unspecified"
+    ET.SubElement(object_element, "truncated").text = "0"
+    ET.SubElement(object_element, "difficult").text = "0"
+    
+    # Add bounding box coordinates
+    bndbox = ET.SubElement(object_element, "bndbox")
+    ET.SubElement(bndbox, "xmin").text = str(box[0])
+    ET.SubElement(bndbox, "ymin").text = str(box[1])
+    ET.SubElement(bndbox, "xmax").text = str(box[0] + box[2])
+    ET.SubElement(bndbox, "ymax").text = str(box[1] + box[3])
+    
+    # Append object to root and save the updated XML file
+    root.append(object_element)
+    tree.write(xml_file_path, encoding="utf-8", xml_declaration=True)
+
+
 
 def save_proposals(images, annotations, names, output_dir, num_proposals, iou_threshold, algorithm='SelectiveSearch'):
-    for i, (image, gt_boxes) in enumerate(zip(images, annotations)):
+    for i, (image, gt_boxes) in enumerate(tqdm(zip(images, annotations), total=len(images), desc="processing images")):
         if algorithm == 'SelectiveSearch':
             proposals = SelectiveSearch(image, num_proposals, mode='quality')
         elif algorithm == 'EdgeDetection':
@@ -15,34 +75,42 @@ def save_proposals(images, annotations, names, output_dir, num_proposals, iou_th
         
         img_dir = os.path.join(output_dir, str(names[i])[:-4])
         os.makedirs(img_dir, exist_ok=True)
+        # Create xml file for the image proposals
+        xml_file_path = initialize_xml_file(img_dir, names[i], image.shape)
+
         pos_dir = os.path.join(img_dir, 'positive')
         neg_dir = os.path.join(img_dir, 'background')
         os.makedirs(pos_dir, exist_ok=True)
         os.makedirs(neg_dir, exist_ok=True)
 
-        for j, proposal in enumerate(proposals):
+        for j, proposal in enumerate(tqdm(proposals, total=len(proposals), desc="saving proposals")):
             x, y, w, h = proposal
             crop_img = image[y:y+h, x:x+w]
             max_iou = max(compute_IoU(proposal, gt_box) for gt_box in gt_boxes)
             label = 'positive' if max_iou >= iou_threshold else 'background'
-            
+            # TODO: Add coordinetaes to the xml file
+            add_object_to_xml(xml_file_path, proposal, f'img-{i+1}_{j}.jpg', label)
             save_dir = pos_dir if label == 'positive' else neg_dir
             cv2.imwrite(os.path.join(save_dir, f'img-{i+1}_{j}.jpg'), crop_img)
         # Free up memory
         img = img_dir + '.jpg'
         xml = img_dir + '.xml'
-        os.remove(img) # TODO: Uncomment this line when ready to run the full dataset
+        # os.remove(img) # TODO: Uncomment this line when ready to run the full dataset
         # Move the xml to the new directory?? 
         # shutil.move(xml, pos_dir)
     print(f"Saved cropped proposals in {output_dir}!")
 
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-folder_path = os.path.join(script_dir, 'Potholes', 'annotated-images', 'test')
+folder_path = os.path.join(script_dir, 'Temp', 'annotated-images', 'train')
 images, annotations, names = read_images_and_xml(folder_path)
 
 # # Test on a subset of the data
 # images = images[:1]
 # annotations = annotations[:1]
 # names = names[:1]
+save_proposals(images, annotations, names, output_dir=folder_path, num_proposals=1000, iou_threshold=0.6, algorithm='SelectiveSearch')
+
+folder_path = os.path.join(script_dir, 'Temp', 'annotated-images', 'test')
+images, annotations, names = read_images_and_xml(folder_path)
 save_proposals(images, annotations, names, output_dir=folder_path, num_proposals=1000, iou_threshold=0.6, algorithm='SelectiveSearch')
